2c9c6078039d422c10a9e63bb80a642053c3edf2
Carlos <cgranillo2005@gmail.com>
2026-04-28 10:51:36 -0700

    Styling - video player layout an controls

--- src/VideoPlayer.jsx
+++ src/VideoPlayer.jsx
@@ -1,3 +1,4 @@
+import './VideoPlayer.css'
 import { useState, useRef } from 'react' // Importing useState and useRef functions from React library
 
 function VideoPlayer(){ // Creating a function called VideoPlayer that creates a reusable component that renders a single video player with upload, speed control and bookmark functionality
@@ -21,17 +22,35 @@ function VideoPlayer(){ // Creating a function called VideoPlayer that creates a
     }
 
     return ( 
-        <div>
+        <div className = "video-player">
             <input type = "file" accept = "video/*" onChange = {handleFileChange}/>
-            <video ref = {videoRef} src = {videoSrc} controls />
+            <video ref = {videoRef} src = {videoSrc} controls className = "video-elemet"/>
+            <div className = "speed-controls">
             <button onClick = {() => handleSpeedChange(0.25)}>0.25x</button>
             <button onClick = {() => handleSpeedChange(0.50)}>0.5x</button>
             <button onClick = {() => handleSpeedChange(0.75)}>0.75x</button>
             <button onClick = {() => handleSpeedChange(1.0)}>1x</button>
-            <button onClick = {() => handleBookmark()}>Bookmark</button>
+            </div>
+            <div className = "bookmark-section">
+                <button className = "bookmark-btn onClick = {() => handleBookmark()}>+ Bookmark">Bookmark</button>
+                <ul>
+                    {bookmarks.map((time, index) => (
+                        <li key = {index}>
+                            <button onClick = {() => videoRef.current.currentTime = time}>
+                                {time.toFixed(2)}s
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
     // Our functions returns an input var that accets videos, that has 4 buttons to handle the speed, and a bookmark creation button. ref = {videoRef} which connects the video elemet to the ref so JS can control it
+    // <ul></ul> tells our code and website that a list will live in this block of code
+    // {bookmarks.map((time, index) => ( here we are creating a for loop that is using the array bookmarks, the for loop takes the time the bookmark was created and index Q: What is index in this case? A: Index is provided by JS, which takes note of the pos of each item in the array
+    // <li></li> Here is the start of our list. Q: What is the key = {index} doing? A: key = {index} gives each <li></li> a unique tag name - 0, 1, 2, ... - in order to track which iterms changed when re-rendering 
+    // <button></button> start of the button tag. We want the button to set the current time of the video to the time that was previously saved in our bookmark list whenever the button is clicked
+    // {time.toFixed(2)}s is allowing out button to display the time that was saved in the array to two decimal places along with an s to show seconds
 }
 
 export default VideoPlayer // Exports everything in this file into the main app file
